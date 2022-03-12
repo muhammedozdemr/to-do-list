428592914177cf41764f6ae8466fd020cfcc9b41
muhammedozdemr <16701042@mersin.edu.tr>
2022-03-12 12:59:52 +0300

    seed created for user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('A12345678'),
+                'created_at' => Carbon::now()
+            ]);
+
+    }
+}
